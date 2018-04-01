827ef0e373bb4127631cc27e5e421ab04d8c5faf
chagnaudjimmy <chagnaud.jimmy@gmail.com>
2018-04-01 14:24:18 +0200

    v3

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 get_header();
 ?>
-<div class="header" style="background-image: url('<?php echo get_template_directory_uri() ?>/img/bg1.jpg');"></div>
+<div class="header" style="background-image: url('<?php echo get_template_directory_uri() ?>/img/bg3.jpg');"></div>
 	<div class="contentPage container">
 		<div class="row text-center textContact">
 			<div class="col-md-8 col-md-offset-2">
